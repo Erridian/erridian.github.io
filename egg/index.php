83db663a3efd1babf3c669cd670cc8fcb050de4e
Erridian <jeckpot83@gmail.com>
2021-02-22 23:34:15 +0700

    Create index.php

---
+++ egg/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="pragma" content="no-cache" />
+	<meta charset="utf-8" />
+	<title>Арктика и ее освоение</title>
+</head>
+<body>
+	<div>
+		<? include "list.txt; // Считываем из text.htm всё и выводим ?>
+	</div>
+</body>
+</html>
