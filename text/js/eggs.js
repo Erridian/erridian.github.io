4fd3fb8dcca96a6274e34fb88c164b803abaf8b4
Erridian <jeckpot83@gmail.com>
2023-03-12 02:24:22 +0700

    1

---
+++ text/js/eggs.js
@@ -0,0 +1,5 @@
+document.addEventListener("keydown", function(event) {
+        if(event.code == "KeyU" && event.altKey && event.ctrlKey && event.shiftKey) {
+          alert("Это была пасхалОчка! Сайт сделан человеком, который нихрена не понимает в написании кода, даже HTML верстке. О чем тут даже говорить про JavaScript");
+        }
+      });
